81812277aeed85bdc2a4192d074a25ba7dfc90fc
fREEZY503 <egorkss@gmail.com>
2021-06-18 17:01:04 +0300

    Added PR

--- packer/files/deploy.sh
+++ packer/files/deploy.sh
@@ -1,5 +1,10 @@
-sudo apt install -y git
+#!/bin/bash
+
+apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit
 bundle install
 puma -d
+
+#echo "puma -d" >> /etc/rc.local 
+#echo "exit 0"
